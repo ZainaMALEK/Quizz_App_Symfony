46fe8cecfd203e23ea31d29e2dba86cbfe14c8d8
Zaina <malek.zaina@gmail.com>
2018-12-02 22:16:51 +0100

    crud on question, category

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -17,8 +17,13 @@ class CategoryController extends AbstractController
      */
     public function index()
     {
+
+        $categories = $this->getDoctrine()
+                           ->getRepository(Category::class)
+                           ->findAll();
+
         return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+            'categories' => $categories
         ]);
     }
 
@@ -46,4 +51,47 @@ class CategoryController extends AbstractController
           ]);
 
     }
+
+    /**
+     * @Route("/category/edit/{id}", name="category_edit")
+     */
+
+      public function edit($id, Request $request)
+        {
+          $em = $this->getDoctrine()->getManager();
+          // récupération des données
+          $category = $em
+            ->getRepository(Category::class)
+            ->find($id);
+
+          $form = $this->createForm(CategoryType::class, $category);
+
+          $form->handleRequest($request);
+          if ($form->isSubmitted()) {
+            // modifie l'object category avec les données postées
+            $country = $form->getData();
+            $em->flush();
+            return $this->redirectToRoute('category');
+          }
+          return $this->render('category/edit.html.twig', [
+            'form' => $form->createView()
+          ]);
+        }
+
+        /**
+         * @Route("/category/delete{id}", name="category_delete")
+         */
+        public function delete($id)
+        {
+            $em = $this->getDoctrine()->getManager();
+            $category = $this->getDoctrine()
+              ->getRepository(Category::class)
+              ->find($id);
+            $em->remove($category);
+            $em->flush();
+            return $this->redirectToRoute('category');
+        }
+
+
+
 }
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -11,15 +11,39 @@ use App\Entity\Question;
 
 class QuestionController extends AbstractController
 {
-    /**
-     * @Route("/question", name="question")
-     */
-    public function index()
-    {
-        return $this->render('question/index.html.twig', [
-            'controller_name' => 'QuestionController',
-        ]);
-    }
+  /**
+   * @Route("/question", name="question")
+   */
+  public function index()
+  {
+
+      $questions = $this->getDoctrine()
+                         ->getRepository(Question::class)
+                         ->findAll();
+
+      return $this->render('question/index.html.twig', [
+          'questions' => $questions
+      ]);
+  }
+
+
+
+  /**
+   * @Route("/question/{id}", name="question_detail")
+   */
+  public function detail($id)
+  {
+
+    $question = $this->getDoctrine()
+      ->getRepository(Question::class)
+      ->find($id);
+
+      return $this->render('question/detail.html.twig', [
+          'question' => $question
+      ]);
+  }
+
+
 
 
     /**
@@ -46,4 +70,47 @@ class QuestionController extends AbstractController
           ]);
 
     }
+
+
+    /**
+     * @Route("/question/edit/{id}", name="question_edit")
+     */
+
+      public function edit($id, Request $request)
+        {
+          $em = $this->getDoctrine()->getManager();
+          // récupération des données
+          $question = $em
+            ->getRepository(Question::class)
+            ->find($id);
+
+          $form = $this->createForm(QuestionType::class, $question);
+
+          $form->handleRequest($request);
+          if ($form->isSubmitted()) {
+            // modifie l'object category avec les données postées
+            $question = $form->getData();
+            $em->flush();
+            return $this->redirectToRoute('question');
+          }
+          return $this->render('question/edit.html.twig', [
+            'form' => $form->createView()
+          ]);
+        }
+
+        /**
+         * @Route("/question/delete{id}", name="question_delete")
+         */
+        public function delete($id)
+        {
+            $em = $this->getDoctrine()->getManager();
+            $question = $this->getDoctrine()
+              ->getRepository(Question::class)
+              ->find($id);
+            $em->remove($question);
+            $em->flush();
+            return $this->redirectToRoute('question');
+        }
+
+
 }
